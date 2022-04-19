78f15ac5adb5ab93fa6cee190b7d3e13e51a8818
mainak-wq <mainakdeb1998@gmail.com>
2022-04-19 17:44:27 +0530

    with-sql django

---
+++ base/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+	return render(request, "home.html")
+
+def projects(request):
+	return render(request, "projects.html")
+
+def contact(request):
+	return render(request, "contact.html")
+
+def registration(request):
+	return render(request, "registration.html")
